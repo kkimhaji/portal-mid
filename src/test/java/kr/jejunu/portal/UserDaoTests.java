86df2a72c93b46fdad8447ace968bd3fb02987b9
하지현 <kkim.haji@gmail.com>
2022-04-22 16:48:19 +0900

    8

--- src/test/java/kr/jejunu/portal/UserDaoTests.java
+++ src/test/java/kr/jejunu/portal/UserDaoTests.java
@@ -1,5 +1,6 @@
 package kr.jejunu.portal;
 
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -44,27 +45,36 @@ public class UserDaoTests {
         assertThat(insertedUser.getPassword(), is(password));
     }
 
-//    @Test
-//    public void testGetHalla() throws SQLException, ClassNotFoundException {
-//        Integer id = 1;
-//        UserDao userDao = new UserDao(new HallaConnectionMaker());
-//        User user = userDao.get(id);
-//        assertThat(user.getId(), is(id));
-//        assertThat(user.getName(), is(name));
-//        assertThat(user.getPassword(), is(password));
-//    }
-//
-//    @Test
-//    public void insertHalla() throws SQLException, ClassNotFoundException {
-//        UserDao userDao = new UserDao(new HallaConnectionMaker());
-//        User user = new User();
-//        user.setName(name);
-//        user.setPassword(password);
-//        userDao.insert(user);
-//        assertThat(user.getId(), greaterThan(0));
-//
-//        User insertedUser = userDao.get(user.getId());
-//        assertThat(insertedUser.getName(), is(name));
-//        assertThat(insertedUser.getPassword(), is(password));
-//    }
+    @Test
+    public void update() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        String updatedName="HJH";
+        String updatedPassword="1111";
+        user.setName(updatedName);
+        user.setPassword(updatedPassword);
+        userDao.update(user);
+
+        User updatedUser=userDao.get(user.getId());
+
+        assertThat(updatedUser.getName(), is(updatedName));
+        assertThat(updatedUser.getPassword(), is(updatedPassword));
+    }
+
+    @Test
+    public void delete() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.get(user.getId());
+        assertThat(deletedUser, IsNull.nullValue());
+    }
+
 }
